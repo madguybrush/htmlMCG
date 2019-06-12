0d349f1922e337a81494a6d8a74a1c6227213ba2
MaDevops <mathieu.dussault@gmail.com>
2019-06-12 16:04:41 +0200

    V0.6

--- js/script.js
+++ js/script.js
@@ -11,9 +11,7 @@
 		window_height = $( window ).height();
 
 
-			hauteurheader = $('header').height();
-			$('#bienvenue').css('margin-top', hauteurheader);
-
+		
 
 
 	if(window_width > 1297){
@@ -38,6 +36,13 @@
 		$('.arrow-downjaune').css('border-right', border); 
 
 
+		//largeurblocparent = $('.arrow-downblanc').parent().width();
+		largeurblocparent = $('.blocblanc').outerWidth();
+		border = largeurblocparent/2 + "px solid transparent";
+		$('.arrow-downblanc').css('border-left', border); 
+		$('.arrow-downblanc').css('border-right', border); 
+
+
 		largeurfiche = $('.bgblanc').parent().width();
 		//console.log(largeurfiche);
 
@@ -45,38 +50,60 @@
 		$('.bgjaune').css('height', largeurfiche); 
 		$('.bgbleu').css('height', largeurfiche); 
 		$('.bgrouge').css('height', largeurfiche); 
+
+
+
+
 		/*$('.bgblanc').parent().height() = largeurfiche;
 		$('.bgjaune').parent().height();*/
 		
 
 		imageheight = $('.imagecabinet').height();
+if(window_width > 768){
 		$('.blocrouge').css('height', imageheight); 
 
 		$('.blocbleu').css('height', imageheight); 
+		$('.blocblanc').css('height', imageheight); 
+
+}
+else{
+			$('.blocrouge').css('height', ''); 
+
+		$('.blocbleu').css('height', ''); 
+		$('.blocblanc').css('height', ''); 
+
+$('.navbar').addClass('navbar-scroll');
+
+}
+
 		
-			if((( window_width < 1024 ) && (window_width > 768)) || (window_width < 550) ){
+			//if((( window_width < 1024 ) && (window_width > 768)) || (window_width < 550) ){
+				if((( window_width < 1024 ) && (window_width > 768)) || (window_width < 700) ){
 
 		//		$('.blocrouge').css('height', ""); 
 		//		$('.blocbleu').css('height', ""); 
-console.log("1");
+//console.log("1");
 					$('.blocrouge').css('display', "block"); 
 				$('.blocbleu').css('display', "block"); 
+				$('.blocblanc').css('display', "block"); 
 				$('.blocjaune').parent().removeClass('order-last'); 
 
 			}
 
 			if( window_width > 768 ) {
 							$('.blocjaune').parent().removeClass('order-last'); 
-							console.log("2");
+				//			console.log("2");
 			}
 
-			if(( window_width >= 1024 ) || ((window_width < 768) && (window_width > 550))){
+			//if(( window_width >= 1024 ) || ((window_width < 768) && (window_width > 550))){
+				if(( window_width >= 1024 ) || ((window_width < 768) && (window_width > 700))){
 
 					$('.blocrouge').css('display', "flex"); 
 				$('.blocbleu').css('display', "flex"); 
 				$('.blocrouge').css('float', "right"); 
 				$('.blocbleu').css('float', "left"); 
-				console.log("3");
+				$('.blocblanc').css('display', "flex");
+			//	console.log("3");
 
 			}
 
@@ -88,14 +115,21 @@ console.log("1");
 				$('.imagecabinet').css('float', "left"); 
 
 
+
 				$('.blocjaune').parent().addClass('order-last'); 
-				console.log("4");
+			//	console.log("4");
 			}
 
 		/*	if(window_width < 550){
 				$('.blocrouge').css('display', "block"); 
 				$('.blocbleu').css('display', "block"); 
 			}*/
+
+
+
+	hauteurheader = $('header').outerHeight();
+			$('#bienvenue').css('margin-top', hauteurheader);
+
 
 	}
 
@@ -126,6 +160,10 @@ $( document ).ready( function() {
 		$('#hp').removeClass('d-none');
 		$('#cabinet').removeClass('nopacity');
 		$('#conseils').removeClass('nopacity');
+		$('#ortho').removeClass('nopacity');
+		$('#coptact').removeClass('nopacity');
+		$('#urgence').removeClass('nopacity');
+		$('#patient').removeClass('nopacity');
 
 		//$('#sousmenuconseils').removeClass('d-none');
 
@@ -181,27 +219,49 @@ console.log("ok");
    
    
 var scroll = new SmoothScroll('#backtotop');
-   
+  var i = 0; 
 window.onscroll = function() {
 
 	scrollFunction();
 
-	hauteurheader = $('header').outerHeight();
+hauteurheader = $('header').outerHeight();
 	//hauteursousmenu = $('#sousmenuconseils').outerHeight();
 	//$('#bienvenue').css('margin-top', hauteurheader);
 
 	if (document.body.scrollTop > hauteurheader || document.documentElement.scrollTop > hauteurheader) {
-		$('#sousmenuconseils').addClass('fixed-top');
+
+			$('.navbar').addClass('animated fadeInDown');
+			$('.navbar').removeClass('fadeInUp');
+		$('.navbar').addClass('navbar-scroll');
+
+	hauteurheader = $('header').outerHeight();
+			$('#sousmenuconseils').addClass('fixed-top');
 		$('#sousmenuconseils').css('top', hauteurheader );
+		//$('header').css('padding', ".5rem" );
+		i = 1;
 	}
 else {
     $('#sousmenuconseils').removeClass('fixed-top');
+    if (i == 1){ 
+    	$('.navbar').removeClass('fadeInDown');
+    	$('.navbar').addClass('animated fadeInUp'); 
+    }
+    if(window_width > 768){
+    	$('.navbar').removeClass('navbar-scroll');
+    }
+	//$('header').css('padding', "1rem" );
     //$('#sousmenuconseils').css('top', hauteurheader);
   }
 
 
 };
 
+/*$( "#sousmenuconseils a" ).click(function() {
+  //alert( "Handler for .click() called." );
+  hauteurheader = $('header').delay( 1800 ).outerHeight();
+ $('#sousmenuconseils').css('top', hauteurheader );
+
+});*/
 
 
 function scrollFunction() {
